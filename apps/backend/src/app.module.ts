91c60e9bff1b32b358b28a235d5477e254ae2ff7
octa-quintero <octa.quinteroo@gmail.com>
2026-04-12 19:35:36 +0200

    Configuracion inicial // winston - morgan - typeormconfig - env.conig - main.ts configuracion inicial

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeormConfig } from './config/typeorm.config';
+import { winstonConfig } from './config/winston-nest.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+    WinstonModule.forRoot(winstonConfig),
+    AuthModule,
+    UsersModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
